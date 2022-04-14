dc0eb0671736e9836f887cf7f7e816b1aa286b99
Brian Steele <brian.steele@hey.com>
2022-04-13 19:59:17 -0700

    adding index page

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,11 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { MetaFunction } from '@remix-run/node'
+import { Links, LiveReload, Meta, Outlet } from '@remix-run/react'
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
+})
 
 export default function App() {
   return (
@@ -23,10 +16,8 @@ export default function App() {
       </head>
       <body>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
