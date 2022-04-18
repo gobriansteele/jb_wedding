a1fb6c268259019efc7762306e538a360f1ca786
Brian Steele <brian.steele@hey.com>
2022-04-17 17:00:00 -0700

    details page

--- app/routes/details.tsx
+++ app/routes/details.tsx
@@ -1,3 +1,18 @@
-export default function Details() {
-  return <p>Welcome to details</p>
+import type { LinksFunction } from '@remix-run/node'
+import stylesUrl from '~/styles/details.css'
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: stylesUrl }]
+}
+
+export default function OurStory() {
+  return (
+    <>
+      <div className="hero-image-wrap">
+        <div className="hero-image-overlay">
+          <h1 className="global-headline">Details</h1>
+        </div>
+      </div>
+    </>
+  )
 }
