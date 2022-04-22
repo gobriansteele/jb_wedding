ab8529dea018ff04c83e0f757b61986dc4756e36
Brian Steele <brian.steele@hey.com>
2022-04-22 08:53:00 -0700

    adding details page info

--- app/routes/details.tsx
+++ app/routes/details.tsx
@@ -1,9 +1,9 @@
-import type { LinksFunction } from "@remix-run/node";
-import stylesUrl from "~/styles/details.css";
+import type { LinksFunction } from "@remix-run/node"
+import stylesUrl from "~/styles/details.css"
 
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: stylesUrl }];
-};
+  return [{ rel: "stylesheet", href: stylesUrl }]
+}
 
 export default function OurStory() {
   return (
@@ -13,6 +13,58 @@ export default function OurStory() {
           <h1 className="global-headline">Details</h1>
         </div>
       </div>
+      <div className="details-wrap">
+        <div className="details-content-wrap">
+          <h2 className="details-section-heading">Where</h2>
+          <h3 className="details-subheading">Venue</h3>
+          <p>
+            Our celebration will take place at the beautiful Tre Posti in the
+            famous Napa Valley. The address is 641 Main Street, St. Helena, CA
+            94574. There is plenty of parking and it is free.
+          </p>
+          <p>
+            The ceremony will be at the Tre Posti Vineyard, followed by a
+            cocktail hour and dinner at the Tre Posti Vignetto and Garden Grove
+            lawn. We'll end the night with a kickin' dance party in the Tre
+            Posti Harvest Room, just a short walk away.
+          </p>
+        </div>
+        <div className="details-content-wrap">
+          <h2 className="details-section-heading">When</h2>
+          <h3 className="details-subheading">Date</h3>
+          <p>Sunday, April 2nd, 2023</p>
+          <h3 className="details-subheading">Time</h3>
+          <p>The ceremony beings at 4:00PM</p>
+          <p>Arrive as early as 3:30PM</p>
+        </div>
+        <div className="details-content-wrap">
+          <h2 className="details-section-heading">How</h2>
+          <h3 className="details-subheading">Travel</h3>
+          <p>
+            For our out of town guests, you can fly into one of the 3 following
+            airports:
+          </p>
+          <ul className="details-airport-list">
+            <li>San Francisco (SFO)</li>
+            <li>Oakland (OAK)</li>
+            <li>Sacramento (SMF)</li>
+          </ul>
+          <p className="bold">
+            ** Hotel information coming in the summer of 2022 **
+          </p>
+          <h3 className="details-subheading">What to wear</h3>
+          <p>
+            The event is semi-formal, but anything you want to wear we are sure
+            will be ok. The bride requests you refrain from wearing a wedding
+            dress (but if that's really all you have to wear, she prefers that
+            to you going naked). Napa spring weather is typically mild (60s -
+            70s), but could get chilly once the sun goes down. The ceremony and
+            dinner will be outside and the reception will be inside, so be sure
+            to bring a sweater or light jacket just in case. And don't forget
+            your sunglasses!
+          </p>
+        </div>
+      </div>
     </>
-  );
+  )
 }
